ecbb17afebb525ea5420dd5468eb02cad36cc74e
Tabuang Jandam <JaTabuang@central.co.th>
2020-02-11 11:05:57 +0700

    Node tran update

---
+++ layouts/default.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+      <input type="text" v-model="dataIN"/>
+     <button v-on:click="checkForm()">Add</button>
+    <div v-for="(row, i) in output" :key="i">
+      <label>{{row.inputname}}</label>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      dataIN:'',
+      output:[]
+    }
+  },
+  methods:{
+    checkForm(){  
+       this.output.push({inputname: this.dataIN})
+    }
+  }
+}
+</script>
+
+<style>
+</style>
